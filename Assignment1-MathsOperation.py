6b74dd0754a3d7ab707e96c27a9031d0b214ab09
mamathalj <50997354+mamathalj@users.noreply.github.com>
2025-12-30 20:43:03 +0530

    Add files via upload

---
+++ Assignment1-MathsOperation.py
@@ -0,0 +1,7 @@
+a=int(input('enter the first number:   '))
+b=int(input('enter the second number:   '))
+
+print('Addition of two numbers is :   ', a+b)
+print('Subtraction of two numbers is :   ', a-b)
+print('Multiplication of two numbers is :   ', a*b)
+print('Division of two numbers is :   ', a/b)
---
+++ assignment1-Greeting.py
@@ -0,0 +1,3 @@
+name=input('Enter your first name: ')
+last_name=input('Enter your last name: ')
+print('Hello', name, last_name, 'You are heartily Invited for the Evening Gettogether at our residence')
